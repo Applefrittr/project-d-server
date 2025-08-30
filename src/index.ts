7eb0504249057c1c85d6bba6e09a5c2e152431c9
applefrittr <jacob.lee.jordan@gmail.com>
2025-08-30 13:04:26 -0400

    socket.io integration -> intial connection setup with client app

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,23 @@
 import app from "./app";
 import http from "http";
-const debug = require("debug")("typescript-express-skelly:server");
+import { Server } from "socket.io";
+const debug = require("debug")("project-d-server:server");
 
 const server = http.createServer(app);
+
+// mount socket.io to http server
+const io = new Server(server, {
+  cors: { origin: "http://localhost:5173" },
+});
+
+// Mount socket handlers on connection
+io.on("connection", (socket) => {
+  console.log("user is connected");
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
 
 server.listen(process.env.PORT || 6969);
 server.on("listening", onListening);
