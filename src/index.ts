9be2db28b6fcf9c879ca3d54587f8b7f43f79fd5
applefrittr <jacob.lee.jordan@gmail.com>
2025-09-01 19:09:44 -0400

    basic socket setup complete

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,16 @@ const server = http.createServer(app);
 
 // mount socket.io to http server
 const io = new Server(server, {
-  cors: { origin: "http://localhost:5173" },
+  cors: {
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+  },
 });
 
 // Mount socket handlers on connection
 io.on("connection", (socket) => {
   console.log("user is connected");
+
+  // Launch Game
 
   socket.on("disconnect", () => {
     console.log("user disconnected");
